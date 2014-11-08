95c90ef2e57864232f93f90fc006ef65d1c159ed
twissell <twissell.development@gmail.com>
2014-11-08 22:24:46 +0100

    added pos param to constructor and make pos property private

--- obscure.py
+++ obscure.py
@@ -1,10 +1,10 @@
 class Camera(object):
 
-    def __init__(self):
-        self.pos = (0, 0)
+    def __init__(self, pos=(0, 0)):
+        self.__pos = (0, 0)
 
     def apply(self):
-        return (-self.pos[0], -self.pos[1])
+        return (-self.__pos[0], -self.__pos[1])
 
     def update(self):
-        self.pos = (self.pos[0] + 2, self.pos[1])
+        self.__pos = (self.__pos[0] + 2, self.__pos[1])
