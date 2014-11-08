e5b7b27f6c7ab6f18ff3a587af3a7c8f49878217
twissell <twissell.development@gmail.com>
2014-11-08 22:29:52 +0100

    added speed property

--- obscure.py
+++ obscure.py
@@ -1,10 +1,14 @@
 class Camera(object):
 
-    def __init__(self, pos=(0, 0)):
-        self.__pos = (0, 0)
+    def __init__(self, pos=(0, 0), speed=(1, 0)):
+        self.__pos = pos
+        self.__speed = speed
 
     def apply(self):
         return (-self.__pos[0], -self.__pos[1])
 
     def update(self):
-        self.__pos = (self.__pos[0] + 2, self.__pos[1])
+        self.__pos = (
+            self.__pos[0] + self.__speed[0],
+            self.__pos[1] + self.__speed[1]
+        )
